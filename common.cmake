15fb3be6b74aee0de870c03eed066e7fe14f7edd
Chris <cmsd2@cantab.net>
2014-11-20 16:32:08 +0000

    allow out-of-source build

--- common.cmake
+++ common.cmake
@@ -1,13 +1,14 @@
 enable_language(ASM-ATT)
 
 function(load_profile ARCH PLATFORM)
-    file(GLOB ARCH_SRCS "kernel/${ARCH}/*.c")
-    file(GLOB PLATFORM_SRCS "kernel/${ARCH}/${PLATFORM}/*.c")
+    message(STATUS "loading profile for ${ARCH}-${PLATFORM}")
+    file(GLOB ARCH_SRCS "${PROJECT_SOURCE_DIR}/kernel/${ARCH}/*.c")
+    file(GLOB PLATFORM_SRCS "${PROJECT_SOURCE_DIR}/kernel/${ARCH}/${PLATFORM}/*.c")
 
-    include("kernel/${ARCH}/toolchain.cmake")
-    include("kernel/${ARCH}/${PLATFORM}/flags.cmake")
+    include("${PROJECT_SOURCE_DIR}/kernel/${ARCH}/toolchain.cmake")
+    include("${PROJECT_SOURCE_DIR}/kernel/${ARCH}/${PLATFORM}/flags.cmake")
 
-    set(PLATFORM_LINKER_SCRIPT "kernel/${ARCH}/${PLATFORM}/link.ld" PARENT_SCOPE)
+    set(PLATFORM_LINKER_SCRIPT "${PROJECT_SOURCE_DIR}/kernel/${ARCH}/${PLATFORM}/link.ld" PARENT_SCOPE)
     set(ARCH_SRCS ${ARCH_SRCS} PARENT_SCOPE)
     set(PLATFORM_SRCS ${PLATFORM_SRCS} PARENT_SCOPE)
 
